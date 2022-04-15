b41ce816ec532740a7018bb8ee453cdafee97644
Yeonji-Lim <view7186@naver.com>
2022-04-15 17:56:36 +0900

    1주차과제

---
+++ 1주차과제/MainTest.java
@@ -0,0 +1,53 @@
+//Basics 5
+public class MainTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("-----Name Class Test-----");
+		System.out.println();
+
+		Name n1 = new Name("Jane", "Black");
+		System.out.println(n1.getInits() + " " + n1.getFirstAndLastName());
+		System.out.println(n1.getLastCommaFirst());
+		System.out.println();
+		
+		Name n2 = new Name("Mary", "Ann", "Smith");
+		System.out.println(n2.getInits() + " " + n2.getFirstAndLastName());
+		System.out.println(n2.getLastCommaFirst());
+		System.out.println();
+
+		System.out.println("-----Name Class Test-----");
+		System.out.println();
+
+		try {
+			
+			CardOrder co1 = new CardOrder("fullNameString");
+		} catch (NameException e) {}
+
+		try {
+			CardOrder co2 = new CardOrder("fullNameString that is over 28");
+		} catch (NameException e) {}
+
+		try {
+			CardOrder co3 = new CardOrder("Yeonj Lim");
+			System.out.println(co3.getInitials());
+			System.out.println(co3.getFormattedName());
+			co3.setBorder('#');
+			co3.setNumCards(10);
+			System.out.println(co3.getSampleCard());
+		} catch (NameException e) {}
+
+		try {
+			CardOrder co3 = new CardOrder("Hyeonseong Donggang Lim");
+			System.out.println(co3.getInitials());
+			System.out.println(co3.getFormattedName());
+			co3.setBorder('@');
+			co3.setNumCards(200);
+			System.out.println(co3.getSampleCard());
+		} catch (NameException e) {}
+
+	}
+
+}
---
+++ 1주차과제/Name.java
@@ -0,0 +1,39 @@
+//Basics 3
+public class Name {
+  private String firstName;
+  private String middleName;
+  private String lastName;
+
+  public Name(String fName, String mName, String lName) {
+		firstName = fName;
+		middleName = mName;
+		lastName = lName;
+  }
+
+  public Name(String fName, String lName) {
+    firstName = fName;
+    middleName = "";
+    lastName = lName;
+  }
+  
+  //getter
+  public String getFirstName() { return firstName; }
+  public String getMiddleName() { return middleName; }
+  public String getLastName() { return lastName; }
+  
+  //setter
+  public void setFirstName(String fn) { firstName = fn; }
+  public void setMiddleName(String mn) { middleName = mn; }
+  public void setLastName(String ln) { lastName = ln; }
+  
+  //custom getter
+  public String getFirstAndLastName() { return firstName + " " + lastName; }
+  public String getLastCommaFirst() { return lastName + ", "+ firstName; }
+
+  public String getInits() {
+    if(middleName.length() == 0) return firstName.substring(0, 1) + lastName.substring(0, 1);
+    else return firstName.substring(0, 1) + middleName.substring(0, 1) + lastName.substring(0, 1);
+  }
+
+}
+
